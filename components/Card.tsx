1fb79d71091e70c02c804b64bf83385cd2d765f2
Aryan Yadav <yadavaryan466@gmail.com>
2024-10-19 21:02:20 +0530

    fixed: edge case in payoffDate.month

--- components/Card.tsx
+++ components/Card.tsx
@@ -13,7 +13,13 @@ const Card = ({calculatedEmi, loanDetails}: any) => {
   }
   const years = duration/12;
   payoffDate.year = (Number.parseInt(startDate.substring(0,4))+years).toFixed(0);
-  payoffDate.month = (Number.parseInt(startDate.substring(5,7)) + (duration % 12)) % 12;
+  const month = (Number.parseInt(startDate.substring(5,7)) + (duration % 12)) % 12;
+  if(month==0){
+    payoffDate.month = 12;
+  }else{
+    payoffDate.month = month;
+  }
+
 
   const loanEst: LoanEstParams = {
     calculatedEmi,
