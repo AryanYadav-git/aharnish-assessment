1ef89bec7fae721468d86279345131f2295dc060
Aryan Yadav <yadavaryan466@gmail.com>
2024-10-19 14:40:58 +0530

    initialised: shadcn and LoanDetailsForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import LoanDetailsForm from "@/components/LoanDetailsForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=""></div>
+    <div className="">
+      <LoanDetailsForm/>
+    </div>
   );
 }
---
+++ components/LoanDetailsForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoanDetailsForm = () => {
+  return (
+    <div>LoanDetailsForm</div>
+  )
+}
+
+export default LoanDetailsForm
