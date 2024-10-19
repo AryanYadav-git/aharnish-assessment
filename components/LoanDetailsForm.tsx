a314316c44b89e61e377ba48ad814d5067e7b95b
Aryan Yadav <yadavaryan466@gmail.com>
2024-10-19 16:32:38 +0530

    update: form state handled

--- components/LoanDetailsForm.tsx
+++ components/LoanDetailsForm.tsx
@@ -1,117 +1,74 @@
 "use client"
 
-    import { zodResolver } from "@hookform/resolvers/zod"
-    import { useForm } from "react-hook-form"
-    import { z } from "zod"
-    
-    import { Button } from "@/components/ui/button"
-    import {
-      Form,
-      FormControl,
-      FormDescription,
-      FormField,
-      FormItem,
-      FormLabel,
-      FormMessage,
-    } from "@/components/ui/form"
-    import { Input } from "@/components/ui/input"
-    
-    const formSchema = z.object({
-      amount: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-      }),
-      interest: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-      }),
-      time: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-      }),
-      date: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-      }),
-    })
-    
+import { Input } from "./ui/input";
+import { useState } from 'react';
+import { Label } from "./ui/label";
 
 const LoanDetailsForm = () => {
+  const [loanDetails, setLoanDetails] = useState({
+    amount: 0,
+    interest: 0,
+    duration: 0,
+    startDate: 0,
+  });
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      amount: "",
-      interest:"",
-      time:"",
-      date:"",
-    },
-  })
- 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setLoanDetails(prevDetails => ({
+      ...prevDetails,
+      [name]: value
+    }));
+  };
+
   return (
-    <div>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-1 bg-[#f4eee6] p-8">
-            <h1>Loan Details</h1>
-            <FormField
-              control={form.control}
-              name="amount"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Loan Amount</FormLabel>
-                  <FormControl>
-                    <Input placeholder="" {...field} className="border-black w-72" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="interest"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Interest Rate</FormLabel>
-                  <FormControl>
-                    <Input placeholder="" {...field} className="border-black" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="time"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Loan term (months)</FormLabel>
-                  <FormControl>
-                    <Input placeholder="" {...field} className="border-black" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="date"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Start date</FormLabel>
-                  <FormControl>
-                    <Input placeholder="" {...field} className="border-black" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
-    </div>
-  )
-}
+    <div className="p-8 bg-[#f4eee6] flex flex-col gap-4">
+      <h1 className="font-semibold text-lg mb-2">Loan details</h1>
 
-export default LoanDetailsForm
+      <div>
+        <Label htmlFor="amount">Loan Amount</Label>
+        <Input 
+          className="border-black" 
+          id="amount" 
+          name="amount" 
+          value={loanDetails.amount} 
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <Label htmlFor="interest">Interest Rate</Label>
+        <Input 
+          className="border-black" 
+          id="interest" 
+          name="interest" 
+          value={loanDetails.interest} 
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <Label htmlFor="duration">Loan Term (months)</Label>
+        <Input 
+          className="border-black w-72" 
+          id="duration" 
+          name="duration" 
+          value={loanDetails.duration} 
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <Label htmlFor="startDate">Start Date</Label>
+        <Input 
+          className="border-black" 
+          id="startDate" 
+          name="startDate" 
+          value={loanDetails.startDate} 
+          onChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoanDetailsForm;
